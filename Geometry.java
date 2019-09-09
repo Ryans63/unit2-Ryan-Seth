48789b545164d9aaa085758ee6e8b0c4533feda7
Michael George <georgem@wlwv.k12.or.us>
2019-09-09 09:23:10 -0700

    tests

---
+++ Geometry.java
@@ -0,0 +1,19 @@
+
+
+public class Geometry
+{
+    public static void main (String[]args)
+    {
+    int sides = 7;
+    int radius = 5; 
+    final double PI = 3.14;
+    
+    System.out.print("A heptagon has "+sides+" Sides /n/n");
+    System.out.print("A decagon has "+(sides+3)+" Sides /n/n");
+    System.out.print("A dodecagon has"+ sides+radius+"Sides"); 
+    
+        
+    
+    }
+
+}
